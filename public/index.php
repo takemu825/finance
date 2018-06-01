f5281bd574d4543db76b1f1f5466402c6d41882c
takemu825 <inner1fire2ice2@yahoo.co.jp>
2018-06-01 14:33:36 +0900

    test

--- public/index.php
+++ public/index.php
@@ -3,12 +3,10 @@
 $method = $_SERVER['REQUEST_METHOD'];
 
 if (empty($_SERVER['REQUEST_URI'])) {
-    //紹介ページを表示
     include('./views/home.php');
     exit;
 }
 
-//スラッシュで区切られたurlを取得します
 $analysis = explode('/', $_SERVER['REQUEST_URI']);
 $call = null;
 
@@ -26,15 +24,11 @@ if ($call == null) {
     exit;
 }
 
-//modelをインクルードします
 if (file_exists('./models/'.$call.'.php')) {
 
     include('./models/'.$call.'.php');
-    //$call名のクラスをインスタンス化します
     $class = new $call();
-    //modelのindexメソッドを呼ぶ仕様です
     $ret = $class->index($analysis);
-    //配列キーが設定されている配列なら展開します
     if (!is_null($ret)) {
         if(is_array($ret)){
             extract($ret);
@@ -42,14 +36,12 @@ if (file_exists('./models/'.$call.'.php')) {
     }
 }
 
-//view/tamplateをインクルードします
 if (file_exists('./views/template/'.$call.'.tpl.html')) {
     include('./views/template/'.$call.'.tpl.html');
 } else {
     include('./views/error.php');
 }
 
-//viewをインクルードします
 if (file_exists('./views/'.$call.'.php')) {
     include('./views/'.$call.'.php');
 } else {
--- public/models/finance.php
+++ public/models/finance.php
@@ -3,6 +3,8 @@
 class finance {
 
     function index($params) {
+        $link = mysqli_connect('127.0.0.1', 'root', 'root', 'test_db1');
+//        var_dump($link);
         return array('str'=>'Hello 家計簿!');
     }
 
