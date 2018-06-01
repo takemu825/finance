2aaa24e4c6ef93c416a926c0f9ce03153c4887b9
takemu825 <inner1fire2ice2@yahoo.co.jp>
2018-06-01 14:38:07 +0900

    test

--- public/models/finance.php
+++ public/models/finance.php
@@ -3,8 +3,6 @@
 class finance {
 
     function index($params) {
-        $link = mysqli_connect('127.0.0.1', 'root', 'root', 'test_db1');
-//        var_dump($link);
         return array('str'=>'Hello 家計簿!');
     }
 
